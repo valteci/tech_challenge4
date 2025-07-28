9dad5d7402b25e9adf00c12abf4bd8084e0c7cce
valteci <valtecijunior@gmail.com>
2025-07-27 22:37:10 -0300

    finalizando endpoint de train

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from src.pipeline import Pipeline
 
 app = Flask(__name__)
-STOCKS = ['ITUB4.SA']
 pipeline = Pipeline()
 
 # retorna a página home
@@ -12,12 +11,37 @@ def home():
 
 
 #treina o modelo e faz deploy com os novos pesos
-@app.route('/train')
+@app.route('/train', methods=['POST'])
 def train():
-    pipeline._download_data(STOCKS, '2010-01-01', '2025-01-01')
+    data = request.get_json(silent=True)
+
+    if not data:
+        return jsonify({"error": "Voce precisa mandar um json valido."}), 400
+
+    if 'stock' not in data:
+        return jsonify({"error": "Campo 'stock' obrigatorio no corpo JSON."}), 400
+    
+    if 'inicio' not in data:
+        return jsonify({"error": "Campo 'inicio' obrigatorio no corpo JSON."}), 400
+    
+    if 'fim' not in data:
+        return jsonify({"error": "Campo 'fim' obrigatorio no corpo JSON."}), 400
+    
+    inicio: str = data['inicio']
+    fim:    str = data['fim']
+    stock:  str = data['stock']
+    
+    if not stock.endswith('.SA'):
+        stock += '.SA'
+
+    pipeline._download_data([stock], inicio, fim)
     pipeline._train_model()
     pipeline.deploy_model()
-    return 'Treino concluído!'
+    
+    return jsonify(
+        {
+            "message": "Treino concluido com sucesso! O modelo foi atualizado!"
+        }), 200
 
 
 # prevê o preço da ação
