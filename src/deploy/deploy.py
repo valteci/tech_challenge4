0581c4e6f4b6fdbe78fde6366574289d20a8cf28
valteci <valtecijunior@gmail.com>
2025-07-28 21:22:16 -0300

    higienizando arquivo deploy.py

--- src/deploy/deploy.py
+++ src/deploy/deploy.py
@@ -8,10 +8,8 @@ import numpy as np
 
 class Deploy:
     def __init__(self, hparams: Hparams):
-        # Carrega o modelo
-        self._hparams = hparams
-
-        self._model = StockLSTM(hparams=hparams)
+        self._hparams   = hparams
+        self._model     = StockLSTM(hparams=hparams)
 
         state_dict = torch.load(
             f'{Train.SAVING_WEIGHTS_PATH}/best_model.pth',
@@ -19,9 +17,7 @@ class Deploy:
         )
 
         self._model.load_state_dict(state_dict)
-
         self._model.to(hparams.device)
-
         self._model.eval()
 
 
@@ -29,18 +25,19 @@ class Deploy:
         closes      = input_data['Close'].astype(float).values
         seq_len     = self._hparams.sequence_length
 
-        # 1) pega só os últimos seq_len valores
+        # pega só os últimos seq_len valores
         seq         = closes[-seq_len:]
 
-        # 2) monta o tensor (1, seq_len, 1)
+        # monta o tensor (1, seq_len, 1)
         x           = torch.tensor(seq, dtype=torch.float32) \
                             .view(1, seq_len, 1) \
                             .to(self._hparams.device)
-        # 3) inferência
+        # inferência
         self._model.to(self._hparams.device).eval()
         with torch.no_grad():
             y_hat = self._model(x)
-        # 4) extrai numpy (reshape em 1D)
+        
+        # extrai numpy (reshape em 1D)
         preds = y_hat.cpu().numpy().reshape(-1)
         return preds
 
