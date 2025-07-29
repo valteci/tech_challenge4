5083d94771419efe5fd27f0a7a889596afa5c572
valteci <valtecijunior@gmail.com>
2025-07-28 21:04:55 -0300

    higienizando arquivo model.py

--- src/train/model.py
+++ src/train/model.py
@@ -6,10 +6,10 @@ class StockLSTM(nn.Module):
     """
     Estrutura LSTM, com fluxo de dados explícito.
     """
-    def __init__(self, hparams: Hparams): # : Hparams):
+    def __init__(self, hparams: Hparams):
         super().__init__()
         
-        # 1) Definir camadas individualmente
+        # 1) Camada LSTM
         self.lstm1 = nn.LSTM(
             input_size=hparams.input_size,
             hidden_size=hparams.hidden_size,
@@ -17,8 +17,10 @@ class StockLSTM(nn.Module):
             batch_first=True
         )
 
+        # Camada de dropout
         self.dp  = nn.Dropout(0.3)
 
+        # Camada de LSTM
         self.lstm2 = nn.LSTM(
             input_size=hparams.hidden_size,
             hidden_size=max(hparams.future_steps * 4, 32),
@@ -26,6 +28,7 @@ class StockLSTM(nn.Module):
             batch_first=True
         )
 
+        # Camada de dropout
         self.dp_out = nn.Dropout(0.1)
 
         # 4) Camada de saída linear
@@ -35,22 +38,13 @@ class StockLSTM(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # Passa pela primeira LSTM, pegando apenas a sequência de saída
-        # out1 tem a forma [batch, seq_len, hidden_size]
+
         out1, _ = self.lstm1(x)
-
-        out1 = self.dp(out1)
-
-        # Passa pela segunda LSTM
-        # out2 tem a forma [batch, seq_len, hidden_size_da_lstm2]
+        out1    = self.dp(out1)
         out2, _ = self.lstm2(out1)
 
-        # Pega a saída do último passo temporal
-        # last_time_step tem a forma [batch, hidden_size_da_lstm2]
         last_time_step = out2[:, -1, :]
+        last_time_step = self.dp_out(last_time_step) # dropout
 
-        last_time_step = self.dp_out(last_time_step)
-
-        # Projeta para a saída final
         # final_out tem a forma [batch, future_steps]
         return self.output_layer(last_time_step)
