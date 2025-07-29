3b3c4e3877bf733359da3b536b6359af21cbd510
valteci <valtecijunior@gmail.com>
2025-07-28 21:11:14 -0300

    higienizando arquivo hyperparamater.py

--- src/train/hyperparamater.py
+++ src/train/hyperparamater.py
@@ -9,7 +9,7 @@ from pydantic import (
 )
 
 class Hparams(BaseModel):
-    # Configurações globais do modelo (opcionais)
+    # Configurações globais do modelo
     model_config = ConfigDict(
         extra="forbid",# erro se tentar passar campo desconhecido
         strict=True    # conversão de tipos implícita é impedida
@@ -20,18 +20,18 @@ class Hparams(BaseModel):
         description="Lista de nomes das features de entrada"
     )
 
-    hidden_size:    int   = Field(50,  gt=0)
-    num_layers:     int   = Field(2,   gt=0)
-    dropout:        float = Field(0.2, ge=0.0, lt=1.0)
-    sequence_length:int   = Field(60,  gt=0)
-    batch_size:     int   = Field(32,  gt=0)
+    hidden_size:    int   = Field(50,   gt=0)
+    num_layers:     int   = Field(2,    gt=0)
+    dropout:        float = Field(0.2,  ge=0.0, lt=1.0)
+    sequence_length:int   = Field(60,   gt=0)
+    batch_size:     int   = Field(32,   gt=0)
     learning_rate:  float = Field(1e-3, gt=0)
     weight_decay:   float = Field(1e-5, ge=0)
     n_epochs:       int   = Field(100,  gt=0)
     future_steps:   int   = Field(5,    ge=1)
+    seed:           int   = Field(42,   ge=0)
+    train_size:     float = Field(0.7,  gt=0)
     device: Literal["cuda", "cpu", "mps"] = "cuda"
-    seed:           int   = Field(42, ge=0)
-    train_size:     float = Field(0.7, gt=0)
 
     # ------------------------------------------------------------------ #
     # Validações personalizadas
@@ -42,7 +42,9 @@ class Hparams(BaseModel):
         if v == "cuda" and not torch.cuda.is_available():
             raise ValueError("cuda não está disponível!")
         if v == "mps" and not (
-            hasattr(torch.backends, "mps") and torch.backends.mps.is_available()
+            hasattr(torch.backends, "mps") 
+            and
+            torch.backends.mps.is_available()
         ):
             raise ValueError("mps não está disponível!")
         return v
